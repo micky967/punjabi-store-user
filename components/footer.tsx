770ba56670b4f91c26fd14511385d770279613c6
Manjit Lider <micky967@yahoo.com>
2023-07-18 22:42:31 -0700

    final touches user

---
+++ components/footer.tsx
@@ -0,0 +1,15 @@
+
+
+const Footer = () => {
+    return ( 
+        <footer className="bg-white border-t">
+            <div className="mx-auto py-10">
+                <p className="text-center text-sm text-gray-500 font-bold">
+                    <span className="text-rose-500">&copy; </span> 2023 - Punjabi Store Ltd. All rights reserved.
+                </p>
+            </div>
+        </footer>
+     );
+}
+ 
+export default Footer;
